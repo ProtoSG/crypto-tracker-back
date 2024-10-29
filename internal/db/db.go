134c1104305d9ee4b167e078777de9acaa1d0992
ProtoSG <diegoalbertosalazar177@gmail.com>
2024-10-29 13:06:34 -0500

    feat: add db config

---
+++ internal/db/db.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type DB struct {
+	addr string
+}
+
+func NewDB(addr string) *DB {
+	return &DB{addr}
+}
+
+func (this *DB) initDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", this.addr)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func (this *DB) SetupDB() *sql.DB {
+	db, err := this.initDB()
+	if err != nil {
+		log.Fatalf("Error al conectar a la base de datos: %v", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatalf("Error al hacer ping a la base de datos: %v", err)
+	}
+
+	log.Println("Base de datos conectada exitosamente")
+	return db
+}
