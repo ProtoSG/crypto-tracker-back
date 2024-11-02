5a6eda2aee6d30fedd29750c8b8c0bbb992415b4
ProtoSG <diegoalbertosalazar177@gmail.com>
2024-11-02 17:19:48 -0500

    feat: update quoteRepository

--- cmd/api/quote/domain/quote.repository.go
+++ cmd/api/quote/domain/quote.repository.go
@@ -1,9 +1,11 @@
 package domain
 
 type QuoteRepository interface {
-	Create(*Quote) error
+	Create(quote *Quote) error
 	Read() ([]*Quote, error)
 	ReadByID(id int) (*Quote, error)
+	ReadByIDCrypto(id int) (*Quote, error)
 	Update(quote *Quote) error
+	UpdateByIDCrypto(quote *Quote) error
 	Delete(id int) error
 }
