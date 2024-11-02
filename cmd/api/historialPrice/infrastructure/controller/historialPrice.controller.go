ac6500cb34e2d22f656e0d27faeaa9cea71f3f6b
ProtoSG <diegoalbertosalazar177@gmail.com>
2024-11-02 17:23:06 -0500

    feat: update historialPriceController

--- cmd/api/historialPrice/infrastructure/controller/historialPrice.controller.go
+++ cmd/api/historialPrice/infrastructure/controller/historialPrice.controller.go
@@ -17,13 +17,18 @@ func NewHistorialPriceController(serviceContainer *services.ServiceContainer) *H
 }
 
 func (this *HistorialPriceController) Create(w http.ResponseWriter, r *http.Request) {
-	historialPrice := domain.HistorialPrice{}
+	historialPrice := &domain.HistorialPrice{}
 	if err := utils.ReadJSON(r, historialPrice); err != nil {
 		utils.WriteError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	if err := this.serviceContainer.HistorialPrice.Create.Execute(historialPrice.ID, &historialPrice); err != nil {
+	if err := this.serviceContainer.HistorialPrice.Create.Execute(
+		historialPrice.ID,
+		historialPrice.IDCrypto,
+		historialPrice.Price,
+		historialPrice.Date,
+	); err != nil {
 		utils.WriteError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
@@ -74,6 +79,29 @@ func (this *HistorialPriceController) ReadByID(w http.ResponseWriter, r *http.Re
 	}
 }
 
+func (this *HistorialPriceController) ReadByIDCrypto(w http.ResponseWriter, r *http.Request) {
+	id, err := utils.GetID(w, r)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	historialPrice, err := this.serviceContainer.HistorialPrice.ReadByIDCrypto.Execute(id)
+	if err != nil {
+		if _, ok := err.(*utils.EntityNotFound); ok {
+			utils.WriteError(w, http.StatusNotFound, err.Error())
+		} else {
+			utils.WriteError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	if err := utils.WriteJSON(w, http.StatusOK, historialPrice); err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
+
 func (this *HistorialPriceController) Update(w http.ResponseWriter, r *http.Request) {
 	id, err := utils.GetID(w, r)
 	if err != nil {
@@ -81,13 +109,44 @@ func (this *HistorialPriceController) Update(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	historialPrice := domain.HistorialPrice{}
+	historialPrice := &domain.HistorialPrice{}
 	if err := utils.ReadJSON(r, historialPrice); err != nil {
 		utils.WriteError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	if err := this.serviceContainer.HistorialPrice.Update.Execute(id, &historialPrice); err != nil {
+	if err := this.serviceContainer.HistorialPrice.Update.Execute(id, historialPrice); err != nil {
+		if _, ok := err.(*utils.EntityNotFound); ok {
+			utils.WriteError(w, http.StatusNotFound, err.Error())
+		} else {
+			utils.WriteError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	data := map[string]string{
+		"message": "HistorialPrice actualizado",
+	}
+	if err := utils.WriteJSON(w, http.StatusOK, data); err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
+
+func (this *HistorialPriceController) UpdateByIDCrypto(w http.ResponseWriter, r *http.Request) {
+	id, err := utils.GetID(w, r)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	historialPrice := &domain.HistorialPrice{}
+	if err := utils.ReadJSON(r, historialPrice); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := this.serviceContainer.HistorialPrice.UpdateByIDCrypto.Execute(id, historialPrice); err != nil {
 		if _, ok := err.(*utils.EntityNotFound); ok {
 			utils.WriteError(w, http.StatusNotFound, err.Error())
 		} else {
