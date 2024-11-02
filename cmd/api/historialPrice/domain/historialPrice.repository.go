230a2ac80fbe8f62a3745bd62635313ccd90d385
ProtoSG <diegoalbertosalazar177@gmail.com>
2024-11-02 17:22:51 -0500

    feat: update historialPriceRepository

--- cmd/api/historialPrice/domain/historialPrice.repository.go
+++ cmd/api/historialPrice/domain/historialPrice.repository.go
@@ -4,6 +4,8 @@ type HistorialPriceRepository interface {
 	Create(*HistorialPrice) error
 	Read() ([]*HistorialPrice, error)
 	ReadByID(id int) (*HistorialPrice, error)
+	ReadByIDCrypto(id int) (*HistorialPrice, error)
 	Update(historialPrice *HistorialPrice) error
+	UpdateByIDCrypto(historialPrice *HistorialPrice) error
 	Delete(id int) error
 }
