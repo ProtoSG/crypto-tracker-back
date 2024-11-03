90a1e3ee047ce95fc60bc6bdb6bce02af4e8c184
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-11-03 10:01:49 -0500

    feat: update cryptoRepository

--- cmd/api/crypto/domain/crypto.repository.go
+++ cmd/api/crypto/domain/crypto.repository.go
@@ -2,7 +2,7 @@ package domain
 
 type CryptoRepository interface {
 	Create(*Crypto) error
-	Read() ([]*Crypto, error)
+	Read() ([]*CryptoResponse, error)
 	ReadByID(id int) (*Crypto, error)
 	ReadByName(name string) (*Crypto, error)
 	Update(crypto *Crypto) error
